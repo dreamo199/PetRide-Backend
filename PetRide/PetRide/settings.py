f4267e1ed05538056914d2eecf9e4bb1a4246e12
dreamo199 <alagbaosokelvin@gmail.com>
2026-03-19 23:44:46 -0500

    Fixed database config

--- PetRide/PetRide/settings.py
+++ PetRide/PetRide/settings.py
@@ -119,12 +119,16 @@ ASGI_APPLICATION = 'PetRide.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.getenv("DB_NAME"),
+        # 'USER': os.getenv("DB_USER"),
+        # 'PASSWORD': os.getenv("DB_PASSWORD"),
+        # 'HOST': os.getenv("DB_HOST"),
+        # 'PORT': os.getenv("DB_PORT"),
+        dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+        )
     }
 }
 
