fa1c5fac68a677fa7d21f02cfde7ef6a00b80455
dreamo199 <alagbaosokelvin@gmail.com>
2026-03-24 23:22:28 -0500

    Changed debug value

--- PetRide/PetRide/settings.py
+++ PetRide/PetRide/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY") or os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get('DEBUG') or os.getenv('DEBUG', 'False')
+DEBUG = os.environ.get('DEBUG', 'False') or os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = ["*"]
 
